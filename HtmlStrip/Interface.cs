f51c81c2fb91664dffacd8151972cf6b6f224a99
Francisco <franciscoluispontes@hotmail.com>
2020-11-14 20:53:43 +0000

    files missing

---
+++ HtmlStrip/Interface.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace HtmlStrip
+{
+    public static class Interface
+    {
+        public static void LoadUI()
+        {
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("------------------HtmlStrip------------------");
+            Console.WriteLine("Nome dos elementos do grupo");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Que método deseja usar para extrair o texto do Html desejado?");
+            Console.WriteLine("1. Através do Url de uma página Web");
+            Console.WriteLine("2. Ficheiro Local");
+            Console.WriteLine("3. Introduzir conteúdo HTML manualmente");
+            Console.WriteLine("");
+
+            GetInput();
+        }
+
+        private static void LoadSecondScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("------------------HtmlStrip------------------");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Escolher modo de visualização do texto extraído do HTML:");
+            Console.WriteLine("1. Visualizar texto na consola");
+            Console.WriteLine("2. Extrair para um ficheiro de texto");
+            Console.WriteLine("");
+        }
+
+        public static void GetInput()  //Escolhe a ação a fazer
+        {
+            string option = Methods.ReadInput();
+            string data;
+            switch (option)
+            {
+                case "1":
+                    data = GetHtmlByUrl();
+                    break;
+                case "2":
+                    data = GetHtmlByPath();
+                    break;
+                case "3":
+                    data = GetHtmlByInput();
+                    break;
+                default:
+                    data = "";
+                    Console.WriteLine("Selecione uma das opções anteriores.");
+                    GetInput();
+                    break;
+            }
+            //Methods.ExtractHtml(data);
+            LoadSecondScreen();
+            ExtrationMode(data);
+        }
+
+        public static void ExtrationMode(String data)
+        {
+            string option = Methods.ReadInput();
+            string HtmlStriped = Methods.ExtractHtml(data);
+            string HtmlStripedWithoutEmptySpaces = Methods.RemoveWhiteSpaces(HtmlStriped);
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine(HtmlStripedWithoutEmptySpaces);
+                    break;
+                case "2":
+                    //Extrair "HtmlStriped" para o ficheiro de texto
+                    //chamar função do Leandro
+                    break;
+                default:
+                    Console.WriteLine("Selecione uma das opções anteriores.");
+                    ExtrationMode(data);
+                    break;
+            }
+
+        }
+
+        public static String GetHtmlByUrl()  //executa 1º opção
+        {
+            Console.WriteLine("Escreva o URL desejado");
+            Console.Write("? ");
+            string urlInput = Console.ReadLine();
+            string urlData = Methods.GetUrlData(urlInput);
+            return urlData;
+
+        }
+
+        public static String GetHtmlByPath() //executa 2º opção
+        {
+            Console.WriteLine("Escreva o caminho do ficheiro Html");
+            Console.Write("? ");
+            string pathInput = Console.ReadLine();
+            
+            //dar return da função do Leandro que extrai o HTML através do caminho
+            return pathInput;
+        }
+
+        public static String GetHtmlByInput() //executa 3º opção
+        {
+            return Methods.OptionPasteHTML();
+
+        }
+    }
+}
---
+++ HtmlStrip/Methods.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HtmlStrip
+{
+    public static class Methods
+    {
+
+        public static String GetUrlData(String urlAddress)  //dá return do HTML inteiro
+        {
+            String urlData;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                    readStream = new StreamReader(receiveStream);
+                else
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+
+                urlData = readStream.ReadToEnd(); //a partir daqui podemos processar a string que recebemos e apenas ficar com o text que está dentro das tags de texto
+                response.Close();
+                readStream.Close();
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível estabelecer uma ligação com sucesso ao Url inserido.");
+                urlData = "";
+            }
+            return urlData;
+        }
+
+        public static String ReadInput()
+        {
+            Console.Write("?");
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        public static String ExtractHtml(String urlData)
+        {
+            String htmlAfterStrip = Regex.Replace(urlData, "<.*?>", "");
+
+            return htmlAfterStrip;
+        }
+
+        public static String RemoveWhiteSpaces( String value )
+        {
+            return Regex.Replace(value, @"\s+", "");
+        }
+
+        public static String OptionPasteHTML() 
+        {
+            string allLines = "";
+            string line = "";
+            ConsoleKeyInfo keyPressed;
+            Console.WriteLine("Paste your code, and then on an empty line press 'END' key");
+            while ( true )
+            {   
+                keyPressed = Console.ReadKey(true);
+                if ( keyPressed.Key == ConsoleKey.End) break;
+                line = Console.ReadLine();
+                allLines += line;
+            }
+            return allLines;
+        }
+    }
+}
